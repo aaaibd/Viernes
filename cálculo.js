cf176309491ecfcd5f1776c2b1ad98717d3d228d
aaaibd <128844981+aaaibd@users.noreply.github.com>
2023-07-13 17:31:59 -0400

    a

---
+++ cálculo.js
@@ -0,0 +1,38 @@
+
+const base = document.getElementById("Base");
+const altura = document.getElementById("Altura");
+
+Atriangulo = (base * altura)/2
+
+
+/*  math.pi (pa sacar pi de gratis)
+Perímetro de un círculo:
+2 * Pi * r
+
+
+Perímetro de un triángulo:
+Suma de 3 lados
+
+Área de un círculo:
+Pi * r^2
+
+Área de un triángulo:
+(Base'b'  * altura'h')/ 2
+
+Área de una esfera:
+4 * Pi * r^2
+
+Volumen de una esfera:
+(4/3) * Pi * r^3
+
+
+
+ejemplo de (https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Math/PI)
+
+function calculaCircunferencia(volumen) {
+  return 4/3 * Math.PI * r^3;
+}
+
+calculaCircunferencia(1);  // 6.283185307179586
+
+*/ 
